7358d144497c4496f4c54396a316e430edb69989
Pablo Hermida Mourelle <pablo.hermida.mourelle@altia.es>
2020-02-17 16:25:52 +0100

    Add event click to play button on song

--- src/App/pages/album/Songs.js
+++ src/App/pages/album/Songs.js
@@ -2,12 +2,28 @@ import React from "react";
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 
 class Songs extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.playAudio=this.playAudio.bind();
+    }
+
+    playAudio(url){
+        var audio = document.createElement('audio');
+        audio.src = url;
+        audio.style.display = "none";
+        audio.play();
+        audio.onended = function(){
+          audio.remove();
+        };
+    }
+    
     render(){
         let songs=this.props.data.map((song, i)=>{
             return <div className="song" key={i}>
                 <div className="name">{song.name}</div>
                 <div className="time">{Math.floor(song.seconds/60)}:{song.seconds%60}</div>
-                <div className="play"><PlayArrowIcon /></div>
+                <div className="play" onClick={this.playAudio(song.audio)}><PlayArrowIcon /></div>
             </div>
         });
 
